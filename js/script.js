f6f2b2e64dcfc8b53938eb27509c7cb7155c6855
Yann Guichard <xloadx923@msn.com>
2022-06-15 22:56:07 +0200

    apply background transparent to navList active + layout's li links

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 const burger = document.querySelector('.nav-burger');
+
 burger.addEventListener('click', function(event){
     document.querySelector('body').style = "overflow:hidden;";
     document.querySelector('.nav-list').classList.toggle('active');
-    document.querySelector('.header-container').classList.toggle('active');
-
-    // document.querySelector('.nav-burger i').classList.add("fa");
-    // document.querySelector('.nav-burger i').classList.add("fa-chevron-up");
+    burger.querySelector('i').classList.toggle("fa-bars");
+    burger.querySelector('i').classList.toggle("fa-chevron-up");
 });
