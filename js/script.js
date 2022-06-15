2ea241c44025c3969b381f1968b6b7810a0e2e98
Yann Guichard <xloadx923@msn.com>
2022-06-15 23:07:46 +0200

    State of the overflow is reactivate

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const burger = document.querySelector('.nav-burger');
 
 burger.addEventListener('click', function(event){
-    document.querySelector('body').style = "overflow:hidden;";
+    document.querySelector('body').classList.toggle('active');
     document.querySelector('.nav-list').classList.toggle('active');
     burger.querySelector('i').classList.toggle("fa-bars");
     burger.querySelector('i').classList.toggle("fa-chevron-up");
