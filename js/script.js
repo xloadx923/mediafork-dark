ebf023c848dcc4baeaaa1bae8ae43db991ddc80b
Yann Guichard <xloadx923@msn.com>
2022-06-16 14:37:26 +0200

    create css roots values --dark-black-transparent + --black-color

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const burger = document.querySelector('.nav-burger');
+const services = document.querySelectorAll('#services .cards-list li');
 
 function toggleClass(racine,obj,state){
     if(obj) racine.querySelector(obj).classList.toggle(state);
@@ -13,4 +14,20 @@ function burgerToggle(){
     toggleClass(this,'i',"fa-chevron-up");
 }
 
+function displayService(){
+    services.forEach(function (service){
+        service.addEventListener('click', function(event){
+            let HTML = "";
+            const modal = document.createElement('div');
+            modal.classList.add('modalServices');
+            HTML += `<div class="titleModal">${service.querySelector('.card-ttl').innerText}</div>`;
+            HTML += `<div class="textModal">${service.querySelector('.card-ttl').dataset.text}</div>`;
+            modal.innerHTML = HTML;
+            this.parentElement.appendChild(modal);
+        });
+    });
+}
+
 burger.addEventListener('click', burgerToggle);
+
+displayService();
