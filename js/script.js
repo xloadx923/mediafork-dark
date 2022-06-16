6fb1260ca08b1baa799c3c39a54efb57ab8d1d4e
Yann Guichard <xloadx923@msn.com>
2022-06-16 23:45:43 +0200

    add texts in services + layout desktop calibrate

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function toggleClass(racine,obj,state){
 function burgerToggle(){
     toggleClass(document,'body','active');
     toggleClass(document,'.nav-list','active');
-
+    
     toggleClass(this,'i',"fa-bars");
     toggleClass(this,'i',"fa-chevron-up");
 }
@@ -24,15 +24,16 @@ function displayService(){
             contModal.classList.add('contModalServices');
             const modal = document.createElement('div');
             modal.classList.add('modalServices');
+            const image =  service.querySelector('.card img');
             HTML += `<div class="close"><i class="fa fa-times" aria-hidden="true"></i></div>`;
-            HTML += `<div class="backModalImage"><img src="../img/icon-web.png"></div>`;
+            HTML += `<div class="backModalImage"><img src="${image.getAttribute('src')}"></div>`;
             HTML += `<div class="titleModal">${service.querySelector('.card-ttl').innerText}</div>`;
             HTML += `<div class="textModal">${service.querySelector('.card-ttl').dataset.text}</div>`;
             modal.innerHTML = HTML;
 
             contModal.appendChild(modal);
             this.parentElement.appendChild(contModal);
-            document.querySelector('.close').addEventListener('click', () => contModal.remove());
+            document.querySelector('.contModalServices').addEventListener('click', () => contModal.remove());
         });
     });
 }
