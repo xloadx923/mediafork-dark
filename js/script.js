de150ba63429dab65d784721ca483e2265315b34
Yann Guichard <xloadx923@msn.com>
2022-06-16 14:35:47 +0200

    add dataset-text to .card.ttl

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const burger = document.querySelector('.nav-burger');
 
-
 function toggleClass(racine,obj,state){
-    racine.querySelector(obj).classList.toggle(state);
+    if(obj) racine.querySelector(obj).classList.toggle(state);
+    else racine.classList.toggle(state)
 }
+
 function burgerToggle(){
     toggleClass(document,'body','active');
     toggleClass(document,'.nav-list','active');
+
     toggleClass(this,'i',"fa-bars");
     toggleClass(this,'i',"fa-chevron-up");
 }
